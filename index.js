93ffa12da6f5bc1dcfb9cf223bab5d564d8d4387
Archit <architgargagra@gmail.com>
2023-07-24 04:14:24 +0530

    connect to mongo db and saveing out data  there

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express from "express";
 import fs from "fs";
 import path from "path";
-const users = [];
+import mongoose from "mongoose";
+
+//Note- dont leave and space in the url to connect to mongo
+mongoose
+  .connect("mongodb://127.0.0.1:27017", { dbName: "backend" })
+  .then(() => console.log("connected to db"))
+  .catch((e) => console.log(e));
+
+const msgSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const msg = mongoose.model("messeges", msgSchema);
+
 const app = express();
 // Setting up view engine
 app.use(express.static(path.join(path.resolve(), "public")));
 // Using middleware
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -15,13 +29,15 @@ app.get("/", (req, res) => {
 app.get("/success", (req, res) => {
   res.render("success");
 });
-app.post("/contact", (req, res) => {
-  users.push({ username: req.body.name, email: req.body.email });
+
+app.post("/contact",  async(req, res) => {
+
+  await msg.create({name:  req.body.name,email: req.body.email})
   res.redirect("/success");
 });
 app.get("/users", (req, res) => {
-    res.json({users})
-  });
+  res.json({ users });
+});
 app.listen(5000, () => {
   console.log("Server ki maka bhosda");
 });
